c4a9804af939e0031317b4decb51f14b9bd1f190
Denzel-666 <dorised94@icloud.com>
2025-04-20 23:33:13 -0500

    Create superhero_database.cpp

---
+++ superhero_database.cpp
@@ -0,0 +1,11 @@
+// Obscure Superhero Database - Starter File
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cout << "Welcome to the Obscure Superhero Database!" << endl;
+    // More code coming soon...
+    return 0;
+}
